df1c1e789fbb2b5ccba455a4a82ad82b19a26b38
Boyan Kmetov <bkmetoff@gmail.com>
2023-04-29 11:31:04 +0300

    Reverts breaking change

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,5 +2,5 @@ def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
 
-    expected = "<p>this should fail</p>"
+    expected = "<p>Index page</p>"
     assert expected == response.get_data(as_text=True)
