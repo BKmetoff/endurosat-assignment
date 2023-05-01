230957e947e3c001000866499f8ed4ee39fa96d8
Boyan Kmetov <bkmetoff@gmail.com>
2023-05-01 17:38:46 +0300

    test 2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Index page, test, test</p>"
+    return "<p>Index page, test</p>"
 
 
 @app.route("/health")
--- tests/test_index.py
+++ tests/test_index.py
@@ -2,5 +2,5 @@ def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
 
-    expected = "<p>Index page, test, test</p>"
+    expected = "<p>Index page, test</p>"
     assert expected == response.get_data(as_text=True)
